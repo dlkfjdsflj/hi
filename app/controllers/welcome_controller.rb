0eb7d4db648d1150f27a38240db3471d11a1c3ae
mike <mike-fb-l-y@mail.com>
2017-03-02 19:57:02 +0000

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+    render html: 'hay'
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  #get 'welcome/index'
+  
+  resources :articles
+  
+  root 'welcome#index'
+  
+  get 'pages/about'
+
+  get 'pages/home'
+
+  get 'pages/contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
